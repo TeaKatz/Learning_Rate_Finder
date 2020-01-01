36dc5e54bfa7331d8e90076165c2161b3a8100a6
Panuthep <falcon_270394@hotmail.co.th>
2020-01-01 15:20:41 +0700

    Modify to base on Tensorflow instead of Keras

--- LearningRateFinder.py
+++ LearningRateFinder.py
@@ -1,31 +1,29 @@
 import tempfile
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.callbacks import LambdaCallback
-from tensorflow.keras import backend as K
 
-	
+
 class LearningRateFinder:
-	def __init__(self, model, stopFactor=4, beta=0.98):
+	def __init__(self, model, stop_factor=4, beta=0.98):
 		self.model = model
-		self.stopFactor = stopFactor
+		self.stop_factor = stop_factor
 		self.beta = beta
-		
+
 		self.lrs = None
 		self.lrMult = None
 		self.losses = None
 		self.best_loss = None
 		self.smooth_loss = None
-		self.batch_num = None
-		
+		self.stop_training = None
+
 	def reset(self):
 		self.lrs = []
 		self.lrMult = 1
 		self.losses = []
 		self.best_loss = 1e9
 		self.smooth_loss = 0
-		self.batch_num = 0
-		
+		self.stop_training = False
+
 	def plot(self, start=None, end=None):
 		if start is None:
 			start = 0
@@ -40,72 +38,83 @@ class LearningRateFinder:
 		plt.plot(range(start, end), self.lrs[start:end])
 		plt.ylabel("learning rate")
 		plt.show()
-		
-	def on_batch_end(self, batch, logs):
-		"""
-		Save learning rate in lrs
-		Update learning rate value
-		Calculate smooth loss and save in losses
-		"""
-		# Get current learning rate and save it
-		lr = K.get_value(self.model.optimizer.lr)
+
+	def on_batch_end(self, batch, loss):
+		# Save current learning rate
+		lr = self.model.optimizer.lr.numpy()
 		self.lrs.append(lr)
-		
-		# Calculate smooth loss and save it
-		loss = logs["loss"]
-		self.batch_num += 1
+
+		# Save smooth loss
 		self.smooth_loss = (self.beta * self.smooth_loss) + ((1 - self.beta) * loss)
-		correctedloss = self.smooth_loss / (1 - (self.beta ** self.batch_num))
-		self.losses.append(correctedloss)
-		
+		corrected_loss = self.smooth_loss / (1 - (self.beta ** batch))
+		self.losses.append(corrected_loss)
+
 		# Calculate stop loss
-		stoploss = self.stopFactor * self.best_loss
-		
-		if self.batch_num > 1 and self.smooth_loss > stoploss:
+		stop_loss = self.stop_factor * self.best_loss
+		if batch > 0 and corrected_loss > stop_loss:
 			# Stop training
-			self.model.stop_training = True
+			self.stop_training = True
 			return
-			
-		if correctedloss < self.best_loss:
+
+		if corrected_loss < self.best_loss:
 			# Update best loss
-			self.best_loss = correctedloss
-		
+			self.best_loss = corrected_loss
+
 		# Increase learning rate
 		lr *= self.lrMult
-		K.set_value(self.model.optimizer.lr, lr)
-		
-	def find(self, training_data, start_lr=1e-10, end_lr=1e+1, batch_size=32, epochs=5, sample_size=None, verbose=1):
+		self.model.optimizer.lr = lr
+
+	def find(self, x, y, start_lr=1e-10, end_lr=1e+1, batch_size=32, epochs=5):
 		# Reset parameters
 		self.reset()
-		
-		# If sample size is not defined, use length of training data
-		if sample_size is None:
-			sample_size = len(training_data[0])
-			
+
+		# Get sample size
+		sample_size = x.shape[0]
+
 		# Calculate update rate for learning rate
 		updateTimePerEpoch = np.ceil(sample_size / float(batch_size))
 		updateTimeTotal = epochs * updateTimePerEpoch
 		self.lrMult = (end_lr / start_lr) ** (1.0 / updateTimeTotal)
-			
-		# Save model weights and learning rate, so we can reset it later
+
+		# Create Dataset
+		train_data = tf.data.Dataset.from_tensor_slices((x, y))
+		train_data = train_data.cache()
+		train_data = train_data.shuffle(sample_size).batch(batch_size)
+		train_data = train_data.prefetch(tf.data.experimental.AUTOTUNE)
+
+		# Save model weights and learning rate for restoring after train
 		weightsFile = tempfile.mkstemp()[1]
 		self.model.save_weights(weightsFile)
-		orig_lr = K.get_value(self.model.optimizer.lr)
-		
-		# Create callback function to update learning rate every batch
-		callback = LambdaCallback(on_batch_end=lambda batch, logs: self.on_batch_end(batch, logs))
-		
+		orig_lr = self.model.optimizer.lr.numpy()
+
 		# Run training
-		K.set_value(self.model.optimizer.lr, start_lr)
-		self.model.fit(training_data[0], training_data[1],
-					   batch_size=batch_size,
-					   epochs=epochs,
-					   verbose=verbose,
-					   callbacks=[callback])
-		
+		for epoch in range(epochs):
+			for batch, (x, y) in enumerate(train_data):
+				# Forward propagation
+				with tf.GradientTape() as g:
+					g.watch(x)
+					y_pred = self.model(x)
+					loss = self.model.loss(y, y_pred)
+				# Backpropagation
+				grads = g.gradient(loss, self.model.trainable_variables)
+				# Update model parameters
+				self.model.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+
+				# Update Learning rate
+				self.on_batch_end(batch + 1, loss)
+
+				# Stop training
+				if self.stop_training:
+					# Load model weights back
+					self.model.load_weights(weightsFile)
+					self.model.optimizer.lr = orig_lr
+					return
+			# Report
+			print('Epoch {} Loss {:.4f} LearningRate {:.4f}'.format(epoch + 1, loss, self.model.optimizer.lr.numpy()))
+
 		# Load model weights back
 		self.model.load_weights(weightsFile)
-		K.set_value(self.model.optimizer.lr, orig_lr)
+		self.model.optimizer.lr = orig_lr
 		
 
 if __name__ == "__main__":
